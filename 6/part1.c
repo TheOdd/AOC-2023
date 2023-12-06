f777cb9ed89e0e612e781c5c0c8374291498b176
TheOdd <TheOdd@users.noreply.github.com>
2023-12-06 04:02:22 -0500

    minimize

--- 6/part1.c
+++ 6/part1.c
@@ -3,20 +3,15 @@
 
 #define LEN 4
 
+int count(int n, int m) { return n - 2*(int)((n - sqrt(n*n - 4*m))/2) - 1; }
+
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
     int times[] = {53, 91, 67, 68};
     int distances[] = {250, 1330, 1081, 1025};
-    unsigned long long product = 1;
-    for (int i = 0; i < LEN; i++) {
-        int n = times[i];
-        int m = distances[i];
-        double a = sqrt(n*n - 4*m);
-        a = n - a;
-        a /= 2;
-        int b = a + 1;
-        product *= n - 2*b + 1;
-    }
-    printf("Product: %llu\n", product);
+    int product = 1;
+    for (int i = 0; i < LEN; i++)
+        product *= count(times[i], distances[i]);
+    printf("Product: %d\n", product);
 }
